0bff68f04bcc97586393101211a022f097eceb18
Naxel <46362288+xNaxelx@users.noreply.github.com>
2023-04-02 16:52:16 +0300

    a simple server response check (enter the url in the launch arguments)

--- REST-Client-app.cpp
+++ REST-Client-app.cpp
@@ -1,20 +1,19 @@
-﻿// REST-Client-app.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <iostream>
+#include <cpprest/http_client.h>
+#include <cpprest/base_uri.h>
 
-#include <iostream>
+std::unique_ptr<web::http::client::http_client> client;
 
-int main()
+int main(int argc, char* argv[])
 {
-    std::cout << "Hello World!\n";
+    client = std::unique_ptr<web::http::client::http_client>(new web::http::client::http_client(utility::conversions::to_string_t(argv[1])));
+
+    auto task = client->request(utility::conversions::to_string_t("GET"), utility::conversions::to_string_t("api/v1/token"));
+    task.wait();
+    auto task2 = task.get().extract_json();
+    task2.wait();
+
+    auto output = task2.get().serialize();
+
+    std::wcout << output;
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
